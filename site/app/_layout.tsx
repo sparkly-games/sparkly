8d7425aca2934c6e8aa729ac381850c8a963c959
Riley <rilwag2612@gmail.com>
2026-02-03 17:23:34 +0000

    Add new google verification method.

--- site/app/_layout.tsx
+++ site/app/_layout.tsx
@@ -17,7 +17,7 @@ const HeaderLogo = () => (
   />
 );
 
-export default function RootLayout() {
+export default async function RootLayout() {
   return (
     <BazingaProvider>
       <Head>
@@ -27,11 +27,11 @@ export default function RootLayout() {
         <meta property="twitter:card" content="summary_large_image" />
         <meta property='twitter:title' content='Sparkly Games' />
         <meta property='twitter:description' content='With Sparkly, get ready to game into the future like never before!' />
-        <meta property='twitter:image' content={require('@/assets/images/open_graph_share_preview.png')} />
+        <meta property='twitter:image' content={await require('@/assets/images/open_graph_share_preview.png')} />
         <meta property="og:url" content="https://sparkly.creepers.sbs/" />
         <meta property='og:site_name' content='Sparkly Games' />
         <meta property='description' content='With Sparkly, get ready to game into the future like never before!' />
-        <meta property="og:image" content={require('@/assets/images/open_graph_share_preview.png')} />
+        <meta property="og:image" content={await require('@/assets/images/open_graph_share_preview.png')} />
         <link rel="canonical" href="https://sparkly.creepers.sbs/" />
         <meta name="google-site-verification" content="WtKSIKOGxz7QiYaXQyBKvFKAkOfFQ_NjfYGeZrEt6mI" />
       </Head>

c1b116eaa8e1e4039f4d898fc599469c1904c528
Riley <rilwag2612@gmail.com>
2026-02-10 18:09:56 +0000

    ui overhall and gh fixup

---
+++ site/assets/data/selling.ts
@@ -0,0 +1,13 @@
+// assets/data/selling.ts
+export const SELLING_POINTS = [
+    {
+        emoji: "🥇",
+        title: "Popular",
+        text: "Our site has over 400 users each day, making it the go-to unblocked games destination."
+    },
+    {
+        emoji: "☁️",
+        title: "Download-less",
+        text: "You don't need to worry about downloads or VPNs. Just click and play instantly in your browser."
+    }
+];

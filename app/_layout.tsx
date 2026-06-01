8e486db085697e6a6f227aa9d143ba874bd3e83c
Riley <rilwag2612@gmail.com>
2026-06-01 16:10:02 +0100

    remove clarity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -231,20 +231,6 @@ export default function RootLayout() {
     return () => clearTimeout(timeoutId);
   }, [toast]);
 
-  /* ------------------ CLARITY ------------------ */
-
-  useEffect(() => {
-    if (Platform.OS === 'web') {
-      (function (c: any, l: Document, a: string, r: string, i: string) {
-        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-        const t = l.createElement(r) as HTMLScriptElement;
-        t.async = true; t.src = "https://www.clarity.ms/tag/" + i;
-        const y = l.getElementsByTagName(r)[0];
-        y.parentNode?.insertBefore(t, y);
-      })(window, document, "clarity", "script", "w27gct3xid");
-    }
-  }, []);
-
   /* ------------------ INIT ------------------ */
 
   useEffect(() => {
